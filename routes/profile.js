41109a8ba6036dbffbaccf9d470a011ff2e198eb
Fotima Khamroeva <bonu_9229@bk.ru>
2022-01-12 03:01:10 -0500

    profile page is ready

--- routes/profile.js
+++ routes/profile.js
@@ -8,25 +8,53 @@
 const express = require('express');
 const router  = express.Router();
 
+const renderProfile = function(user, res, error, success) {
+  viewProperties = {
+    user: user,
+    success: success,
+    error: error
+  };
+  res.render("profile", viewProperties)
+}
+
+const queryUser = function(db, userid, callback) {
+  db.query(`SELECT * FROM users WHERE id = ${userid};`)
+  .then(data => {
+    const users = data.rows[0];
+    callback(users, null);
+  })
+  .catch(err => {
+    callback(null, err.message);
+  });
+}
+
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    db.query(`SELECT * FROM users WHERE id = 1;`)
-      .then(data => {
-        const users = data.rows[0];
-        //res.json({ users });
-        const templateVars = {
-          user: users,
-        };
-        console.log(templateVars);
-        res.render("profile", templateVars)
+    queryUser(db, 1, (user, error) => {
+        renderProfile(user, res, error)
       })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
-
   });
+
+  router.post("/", (req, res) => {
+    let error;
+    let success;
+    db.query(
+      `UPDATE users SET name = '${req.body.name}', email = '${req.body.email}' WHERE id = ${req.body.id};`
+    ).then(data => {
+      if (data.rowCount > 0) {
+        success = "Profile updated!"
+      } else error = "Couldn't update profile";
+    }).catch(err => {
+      error = err.message;
+    });
+    queryUser(db, 1, (user, err) => {
+      if (err) {
+        error = err;
+        success = undefined;
+      }
+      renderProfile(user, res, error, success)
+    })
+  })
 
   return router;
 };
