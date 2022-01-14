07380b843d70021253071a1d0da731e3b8f90956
Yunchoel Lee <yun@Yunchoels-MacBook-Air.local>
2022-01-13 19:56:06 -0800

    fix create bugs

--- routes/resources.js
+++ routes/resources.js
@@ -6,11 +6,14 @@ const router  = express.Router();
 const resources = (db) => {
 // GET all resources on root
   router.get('/', (req, res) => {
+    const user_id = req.session.user_id;
+    if (!user_id) {
+      res.redirect('/auth/login/1');
+    }
     db.query('SELECT * FROM resources;')
       .then((response) => {
         const allResources = response.rows;
 
-        const user_id = req.session.user_id;
         db.query(`SELECT * FROM categories WHERE user_id = ${user_id}`)
           .then((catResponse) => {
             // console.log(catResponse.rows);
@@ -22,24 +25,28 @@ const resources = (db) => {
       });
 
     router.get('/new', (req, res) => {
+      console.log(req.params);
       res.render("new_resource");
     });
 
 
     //create the resources with user typed, and redirct to the all resoureces
     router.post('/new', (req, res) => {
+      const id = req.session.user_id;
       const title = req.body.title;
       const url = req.body.url;
       const description = req.body.description;
-      db.query('INSERT INTO resources (url, title, description) VALUES ($1, $2, $3);',[url,title, description])
-        .then(() => {
-          // res.json(response.rows);
-          res.redirect(`/resources/${req.params.id}`);
+      db.query('INSERT INTO resources (user_id, url, title, description) VALUES ($1, $2, $3, $4) returning id;',[id, url,title, description])
+        .then((response) => {
+          const resource_id = response.rows[0].id;
+
+          db.query('INSERT INTO feedbacks (likes, rating, user_id, resource_id) VALUES ($1,$2,$3, $4);' , [0, 0, id ,resource_id])
+          res.redirect(`/resources/`);
         });
     });
     //comments
     router.post('/:id/comments', (req, res) => {
-      console.log("++++++", req.body);
+      // console.log("++++++", req.body);
       db.query('INSERT INTO comments (comments, user_id, resource_id) VALUES ($1,$2,$3)',[req.body.inputBody, 1, req.params.id])
         .then(() => {
           res.redirect(`/resources/${req.params.id}`);
@@ -48,8 +55,7 @@ const resources = (db) => {
 
     //like
     router.post('/:id/like', (req, res) => {
-
-      db.query('UPDATE feedbacks SET likes = $1 where resource_id = $2;' , [Number(req.body.bth) + 1, req.params.id])
+      db.query('UPDATE feedbacks SET likes = $1 where resource_id = $2;',[Number(req.body.bth) + 1, req.params.id])
         .then(() => {
           res.redirect(`/resources/${req.params.id}`);
         });
@@ -57,16 +63,17 @@ const resources = (db) => {
 
     //rating
     router.post('/:id/rating', (req, res) => {
-      db.query('INSERT INTO feedbacks (likes ,rating, user_id, resource_id) values ($1,$2, $3, $4);', [0, req.body.rating, 1, req.params.id])
+      db.query('UPDATE feedbacks SET rating = $1 where resource_id = $2;', [req.body.rating, req.params.id])
         .then(() => {
           res.redirect(`/resources/${req.params.id}`);
         });
+      
     });
 
-    //rating
 
     //see each resource one by one
     router.get('/:id', (req, res) => {
+      console.log(req.params.id)
       db.query('SELECT * FROM resources WHERE id = $1;', [req.params.id])
         .then((response)=> {
           // res.json(response.rows[0]);
@@ -87,19 +94,21 @@ const resources = (db) => {
                     .then((response)=> {
                       console.log(req.params)
                       const categories = response.rows;
-                  
+                      console.log(Number(req.params.id))
 
                       db.query('SELECT likes, rating from feedbacks JOIN resources ON resources.id = resource_id WHERE resource_id = $1;', [req.params.id])
                         .then((response) => {
-                          // console.log("++++", categories)
-                          const likeRating = response.rows[0];
+                          
+                          console.log("++++", response)
+                          const likeRating = response.rows[response.rows.length - 1];
+
                           const allSources = {
                             resourceId: resource.id,
                             title: JSON.stringify(resource.title),
                             description: JSON.stringify(resource.description),
                             comments: comments,
                             username: JSON.stringify(comments.username),
-                            like :JSON.stringify(likeRating.likes),
+                            like :likeRating ? JSON.stringify(likeRating.likes) : 0,
                             rating: Math.round(avgRating),
                             id: req.params.id,
                             name: categories
